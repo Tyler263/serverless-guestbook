0f2640a0476414e53a635103192c3ab772e0ea77
Tyler263 <107279158+Tyler263@users.noreply.github.com>
2022-06-17 17:44:08 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://00988be3.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://dabad8c9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
